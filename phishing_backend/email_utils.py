b8f01f6e492099cccfbf0330cf09ecff78867221
nickih71 <nickih71@gmail.com>
2026-04-30 17:20:27 -0500

    Update email_utils.py

--- phishing_backend/email_utils.py
+++ phishing_backend/email_utils.py
@@ -34,6 +34,7 @@ def send_email(to_email: str, base_url: str, token: str):
 
     try:
         with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+            server.starttls()
             server.login(SMTP_USERNAME, SMTP_PASSWORD)
             server.sendmail(msg["From"], [to_email], msg.as_string())
     except Exception as e:
